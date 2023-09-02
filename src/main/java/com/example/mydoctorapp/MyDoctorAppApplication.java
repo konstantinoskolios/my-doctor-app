aa6d9304479c4193572572eafc12484fb1620248
Konstantinos Kolios <konstantinoskolios@windowslive.com>
2023-09-02 16:54:10 +0300

    [setup] Add the basic structure for the app

---
+++ src/main/java/com/example/mydoctorapp/MyDoctorAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.mydoctorapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyDoctorAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyDoctorAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/mydoctorapp/MyDoctorAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mydoctorapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyDoctorAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
