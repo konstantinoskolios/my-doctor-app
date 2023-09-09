2f489297112859abbe7b596c50553286eaa92a84
Konstantinos Kolios <konstantinoskolios@windowslive.com>
2023-09-09 23:01:27 +0300

    [feature] Add some constants for better maintenance and reduce boilerplate.

--- src/main/java/com/example/mydoctorapp/constants/Constants.java
+++ src/main/java/com/example/mydoctorapp/constants/Constants.java
@@ -1,4 +1,4 @@
-package com.example.mydoctorapp;
+package com.example.mydoctorapp.constants;
 
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
@@ -9,10 +9,13 @@ public class Constants {
     public static final int TAX_NUMBER_LENGTH = 9;
     public static final int PHONE_NUMBER_LENGTH = 10;
     public static final int SOCIAL_SECURITY_NUMBER_LENGTH = 11;
+    public static final String CITIZENS_TEMPLATE_VALUE = "citizens_view";
     public static final String CITIZENS = "citizens";
-    public static final String MAIN = "main";
+    public static final String MAIN_TEMPLATE_VALUE = "main_view";
+    public static final String DOCTOR_TEMPLATE_VALUE = "doctor_view";
     public static final String SORT_ASCENDING = "asc";
     public static final String START_ITEM = "startItem";
     public static final String END_ITEM = "endItem";
     public static final int ONE_VALUE = 1;
+    public static final String REGEX_EMAIL_FORMAT = "^[A-Za-z0-9+_.-]+@mydoctorapp\\.com$";
 }
