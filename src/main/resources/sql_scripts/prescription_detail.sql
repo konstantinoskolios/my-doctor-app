2b18e99612a97ee4434d2d65f4b2ad8a91388a58
Konstantinos Kolios <konstantinoskolios@windowslive.com>
2023-10-02 13:39:43 +0300

    [Feature] Transitioned from SQL scripts to YAML format for Liquibase scripts, removing all redundant SQL scripts.

--- src/main/resources/sql_scripts/prescription_detail.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE IF NOT EXISTS prescription_detail
-(
-    patient_id   bigint,
-    doctor_id    bigint,
-    category     varchar(50)  not null,
-    prescription varchar(100) not null,
-    date         varchar(10)  not null,
-    CONSTRAINT unique_prescription_combo primary key (patient_id, doctor_id, prescription, date)
-);
