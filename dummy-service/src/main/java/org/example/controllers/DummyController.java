ec9b740f889f7a780eed2d356275477bae5d71e7
Konstantinos Kolios <konstantinoskolios@windowslive.com>
2023-10-16 14:50:10 +0300

    [Refactor] Add dummy sample to test if webclient is working.

---
+++ dummy-service/src/main/java/org/example/controllers/DummyController.java
@@ -0,0 +1,15 @@
+package org.example.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/dummy")
+public class DummyController {
+
+    @GetMapping("test")
+    public String helloWorld(){
+        return  "Hello world from another microservice";
+    }
+}
