9abb3ab5b80459d97c2d1a59ba3f9c0916263693
Konstantinos Kolios <kkolios@cognity.gr>
2023-11-10 21:00:26 +0200

    [feature] Add dummy-service for the eureka-client.

--- dummy-service/src/main/java/org/example/DummyServiceApp.java
+++ dummy-service/src/main/java/org/example/DummyServiceApp.java
@@ -2,10 +2,12 @@ package org.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 import java.util.TimeZone;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class DummyServiceApp {
     public static void main(String[] args) {
         TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
