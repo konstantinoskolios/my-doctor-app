e72b31076dc9e8b68179fcb0230d4b58a6ec1a8c
Konstantinos Kolios <kkolios@cognity.gr>
2023-11-11 23:41:05 +0200

    [feature] Proper changes for dummy service to communicate with gateway cloud configs.

--- dummy-service/src/main/java/org/example/controllers/DummyController.java
+++ dummy-service/src/main/java/org/example/controllers/DummyController.java
@@ -1,14 +1,17 @@
 package org.example.controllers;
 
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.awt.*;
+
 @RestController
-@RequestMapping("/api/dummy")
+@RequestMapping(value = "/api/ds")
 public class DummyController {
 
-    @GetMapping("test")
+    @GetMapping("hello")
     public String helloWorld(){
         return  "Hello world from another microservice";
     }
