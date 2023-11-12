4188f0b5b779e75d7ca9718badca2e43c6374fd3
Konstantinos Kolios <kkolios@cognity.gr>
2023-11-12 23:25:00 +0200

    [feature] Add oauth2 authentication for dummy-services

--- dummy-service/src/main/java/org/example/DummyServiceApp.java
+++ dummy-service/src/main/java/org/example/DummyServiceApp.java
@@ -2,7 +2,6 @@ package org.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 import java.util.TimeZone;
