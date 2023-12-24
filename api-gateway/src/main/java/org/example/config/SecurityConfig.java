c3beb348d4219bcc745d234913772c76ca8280ff
Konstantinos Kolios <konstantinoskolios@windowslive.com>
2023-12-24 12:47:40 +0200

    [refactor]: Corrected url config

--- api-gateway/src/main/java/org/example/config/SecurityConfig.java
+++ api-gateway/src/main/java/org/example/config/SecurityConfig.java
@@ -1,25 +1,25 @@
-//package org.example.config;
-//
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
-//import org.springframework.security.config.web.server.ServerHttpSecurity;
-//import org.springframework.security.web.server.SecurityWebFilterChain;
-//
-//@Configuration
-//@EnableWebFluxSecurity
-//public class SecurityConfig {
-//
-//    @Bean
-//    public SecurityWebFilterChain securityFilterChain(ServerHttpSecurity http) {
-//        http
-//                .csrf()
-//                .disable()
-//                .authorizeExchange()
-//                .anyExchange()
-//                .authenticated()
-//                .and().oauth2ResourceServer().jwt();
-//        return http.build();
-//    }
-//
-//}
+package org.example.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@Configuration
+@EnableWebFluxSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityWebFilterChain securityFilterChain(ServerHttpSecurity http) {
+
+        return http.csrf(ServerHttpSecurity.CsrfSpec::disable)
+                .authorizeExchange(ex ->ex.pathMatchers("/eureka/**")
+                        .permitAll()
+                        .anyExchange().authenticated()
+                ).oauth2ResourceServer(oauth -> oauth
+                        .jwt(Customizer.withDefaults())).build();
+    }
+
+}
