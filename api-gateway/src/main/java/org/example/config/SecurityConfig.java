840a48b2dc44566a3074c154ffeefa7b386b1290
Konstantinos Kolios <konstantinoskolios@windowslive.com>
2023-12-27 21:32:47 +0200

    [feature]: v6

--- api-gateway/src/main/java/org/example/config/SecurityConfig.java
+++ api-gateway/src/main/java/org/example/config/SecurityConfig.java
@@ -5,10 +5,12 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.web.reactive.config.CorsRegistry;
+import org.springframework.web.reactive.config.WebFluxConfigurer;
 
 @Configuration
 @EnableWebFluxSecurity
-public class SecurityConfig {
+public class SecurityConfig  implements WebFluxConfigurer {
 
     public static final String DOCTOR_APP_USER = "doctor-app-user";
     public static final String DOCTOR_APP_ADMIN = "doctor-app-admin";
@@ -26,4 +28,12 @@ public class SecurityConfig {
         return serverHttpSecurity.build();
     }
 
+
+    @Override
+    public void addCorsMappings(CorsRegistry corsRegistry) {
+        corsRegistry.addMapping("/**")
+                .allowedOrigins("http://localhost:8500")
+                .allowedMethods("*")
+                .maxAge(3600);
+    }
 }
