86f575c83df36db0cdbd110f94f1e298f200f21e
Konstantinos Kolios <konstantinoskolios@windowslive.com>
2024-01-14 02:09:47 +0200

    [feature]: Added doctorsRelations for patientAccount

--- my-doctor-app-service/src/main/java/com/example/mydoctorapp/entities/PatientAccount.java
+++ my-doctor-app-service/src/main/java/com/example/mydoctorapp/entities/PatientAccount.java
@@ -1,25 +1,30 @@
 package com.example.mydoctorapp.entities;
 
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.PrePersist;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
 import org.springframework.format.annotation.DateTimeFormat;
 
+import java.util.ArrayList;
 import java.util.Date;
-
+import java.util.List;
 
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
 @Entity
 @Table(name = "patient_account")
-    @Builder
+@Builder
 public class PatientAccount {
     @Id
     @Column(unique = true, nullable = false)
@@ -38,8 +43,12 @@ public class PatientAccount {
     @DateTimeFormat(pattern = "dd-MM-yyyy")
     @Column(name = "created_date", nullable = false, updatable = false)
     private Date createdDate;
-    @Column(nullable = false)
-    private String doctorId;
+
+    @OneToMany(mappedBy = "patient", cascade = CascadeType.ALL)
+    @JsonManagedReference
+    @ToString.Exclude
+    @Builder.Default
+    private List<DoctorPatientRelationship> doctorRelationships = new ArrayList<>();
 
     @PrePersist
     protected void onCreate() {
